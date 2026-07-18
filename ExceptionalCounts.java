3a4d9aa468851ddc352bccee79556ab7efa6cb42
3N16MV <108208668+3N16MV@users.noreply.github.com>
2026-07-18 15:10:29 -0700

    Updated the project header to include all project contributers and make grammatical edits.

--- ExceptionalCounts.java
+++ ExceptionalCounts.java
@@ -1,9 +1,12 @@
 //Programmers: Brittany Burson & Alexis Lariviere
 //Module: 4 Problem 1
 //Date: 07/16/2026
-//Email Address: b.burson4211@nu.edu & t.lariviere7089.nu.edu
+//Email Address: m.bruguesamaro@student.nu.edu , b.burson4211@student.nu.edu & t.lariviere7089@student.nu.edu
 //Class: CSC262
-//Description: This program asks the user for a file name and prints the number of characters, words, and lines in the file. It also prints the longest word in the file.
+//Description: This program asks the user for a file name and prints the number of characters, words, and lines in the
+//file. It also prints the longest word in the file.
+
+//Updated project file header to include all project members and fix grammatical errors.
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -36,9 +39,10 @@ public class ExceptionalCounts
                 String[] words = line.trim().split("\\s+");
                 wordCount += words.length;
                 
-                //Alexis: Count lines in the file, increment lineCount for each line read
+                //Count lines in the file, increment lineCount for each line read
                 lineCount++;
 
+                //Fine longest word in the file
                 for (String word : words) {
                     String cleanWord = word.replaceAll("[^a-zA-Z0-9]", "");
                     if (cleanWord.length() > longestWord.length()) {
